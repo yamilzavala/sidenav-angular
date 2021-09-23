c714b910f2a1fb9f44570565368be351af0e6623
yamilzavala <zavalayamil9@gmail.com>
2021-09-23 18:04:40 -0300

    Inputs and fakeData

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  //Fake data
+  fakeContentData = [{
+    subtitleItem: 'Sidenav subheader data',
+    itemContent: [
+      {itemsTitle: 'Sidenav title 01', itemsContent: 'Sidenav content 01'},
+      {itemsTitle: 'Sidenav title 02', itemsContent: 'Sidenav content 02'},
+      {itemsTitle: 'Sidenav title 03', itemsContent: 'Sidenav content 03'},
+    ],
+  }];
+
+  fakeHeader = 'Example Header Title';
+
   constructor() { }
 
   ngOnInit(): void {
