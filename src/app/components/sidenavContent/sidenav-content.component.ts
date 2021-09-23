992e86f114e2b22f2bbc4ab4a62349fe7a28da50
yamilzavala <zavalayamil9@gmail.com>
2021-09-23 17:49:20 -0300

    Iteration among objects

--- src/app/components/sidenavContent/sidenav-content.component.ts
+++ src/app/components/sidenavContent/sidenav-content.component.ts
@@ -3,23 +3,30 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-sidenav-content',
   templateUrl: './sidenav-content.component.html',
-  styleUrls: ['./sidenav-content.component.css']
+  styleUrls: ['./sidenav-content.component.css'],
 })
 export class SidenavContentComponent implements OnInit {
-  @Input() sidenavContent: SidenavContentI[] = [{ 
-      subtitleItem: 'Subtitle', 
-      itemsTitle: ['Title_01', 'Title_02', 'Title_03'], 
-      itemsContent: ['Content_01', 'Content_02', 'Content_03']
-    }];
-  constructor() { }
+  @Input() sidenavContent: SidenavContentI[] = [
+    {
+      subtitleItem: 'Subtitle',
+      itemContent: [
+        {itemsTitle: 'Title_01', itemsContent: 'Content_01'},
+        {itemsTitle: 'Title_02', itemsContent: 'Content_02'},
+        {itemsTitle: 'Title_03', itemsContent: 'Content_03'},
+      ],
+    }
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
 
 export interface SidenavContentI {
   subtitleItem: string;
-  itemsTitle: string[];
-  itemsContent: string[];
+  itemContent: ItemContent[];
+}
+
+export interface ItemContent { 
+  itemsTitle: string;
+  itemsContent: string;
 }
